aa5cbceb60d6d351173b815f75277b059ede0e75
Silvio <silvio.duka@gmail.com>
2020-12-11 21:02:47 +0100

    small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,13 +18,29 @@ export class AppComponent {
     setInterval(() => {
       this.vrijemeSada = new Date();
 
-      nastava.forEach(element => {
-        if (this.nastavaService.isBetween(element.interval, this.vrijemeSada)) {
-          this.skolskiSat = element;
-          this.skolskiSat.odPocetka = this.nastavaService.difference(element.interval.pocetak, this.vrijemeSada);
-          this.skolskiSat.doKraja = this.nastavaService.difference(element.interval.kraj, this.vrijemeSada);
-        }
-      });
+      if (this.vrijemeSada.getDay() > 5) {
+        this.skolskiSat = {
+          interval: { pocetak: { sati: 0, minute: 0 }, kraj: { sati: 0, minute: 0 } },
+          poruka: { kratkaPoruka: 'Nema nastave!!', dugaPoruka: 'Vikendom nema nastave.' },
+          nastavaUTijeku: false,
+          doKraja: 0,
+          odPocetka: 0,
+          odmor: false,
+          velikiOdmor: false,
+          sat: 0,
+          satUTijeku: false,
+          smjena: 'vikend',
+          image: 'clock.jpg',
+        };
+      } else {
+        nastava.forEach(element => {
+          if (this.nastavaService.isBetween(element.interval, this.vrijemeSada)) {
+            this.skolskiSat = element;
+            this.skolskiSat.odPocetka = this.nastavaService.difference(element.interval.pocetak, this.vrijemeSada);
+            this.skolskiSat.doKraja = this.nastavaService.difference(element.interval.kraj, this.vrijemeSada);
+          }
+        });
+      }
     }, 1000);
   }
 }
